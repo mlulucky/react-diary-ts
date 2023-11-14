d26dbd7dd61f85ef7d60e1fb002f389350f51a29
mlulucky <dmswjdans@gmail.com>
2023-11-15 00:52:52 +0900

    feat: header 컴포넌트 props 옵셔널로 변경

--- src/components/MyHeader.tsx
+++ src/components/MyHeader.tsx
@@ -1,9 +1,9 @@
 import MyButton, {ButtonProps} from './MyButton'
 
 type MyHeaderProps = {
-	leftChild : React.ReactNode; // React 컴포넌트, 문자열, 숫자, 배열 등 거의 모든 것을 포함하는 넓은 타입
+	leftChild? : React.ReactNode; // React 컴포넌트, 문자열, 숫자, 배열 등 거의 모든 것을 포함하는 넓은 타입
 	headText : string;
-	rightChild : React.ReactElement<typeof MyButton>; //MyButton 컴포넌트만 허용하기
+	rightChild? : React.ReactElement<typeof MyButton>; //MyButton 컴포넌트만 허용하기
 }
 
 const MyHeader = ({leftChild, headText, rightChild} : MyHeaderProps) => {
