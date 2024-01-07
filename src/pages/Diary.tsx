c1ac301a1628db101365504d7690de0dc1495096
mlulucky <dmswjdans@gmail.com>
2024-01-07 16:06:08 +0900

    fix: 페이지 수정

--- src/pages/Diary.tsx
+++ src/pages/Diary.tsx
@@ -1,14 +1,61 @@
-import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import MyButton from "../components/MyButton";
+import MyHeader from "../components/MyHeader";
+import useDiary from "../hooks/useDiary";
+import { getCurDate } from "../util/getDate";
+import { emotionList } from "../util/emotionList";
 
 const Diary = () => {
-	const {id} = useParams();
-	console.log("id: ", id);
+  const diary = useDiary();
+  const navigate = useNavigate();
 
-	return (
-		<div>
-			<h1>이곳은 Diary 입니다.</h1>
-		</div>
-	)
-}
+  if (!diary) {
+    return <div className="DiaryPage">로딩중입니다...</div>;
+  } else {
+    const emotionData = emotionList.find(
+      (item) => item.emotion_id === diary.id
+    );
+    console.log("emotionData", emotionData);
+    return (
+      <div className="DiaryPage">
+        <MyHeader
+          leftChild={
+            <MyButton text={"<뒤로가기"} onClick={() => navigate(-1)} />
+          }
+          headText={`${getCurDate(new Date(diary.date))} 기록`}
+          rightChild={
+            <MyButton
+              text={"수정하기"}
+              onClick={() => navigate(`/edit/${diary.id}`)}
+            />
+          }
+        />
+        <article>
+          <section>
+            <h3>오늘의 감정</h3>
+            <div
+              className={[
+                "diary_img_wrapper",
+                `diary_img_wrapper_${emotionData?.emotion_id}`,
+              ].join(" ")}
+            >
+              <img src={emotionData?.emotion_img} alt="" />
+              <div className="emotion_descript">
+                {emotionData?.emotion_descript}
+              </div>
+            </div>
+          </section>
+					<section>
+						<h3>오늘의 일기</h3>
+						<div className="diary_content_wrapper">
+							<p>{diary.content}</p>
+						</div>
+					</section>
+				</article>
+				
+      </div>
+    );
+  }
+};
 
 export default Diary;
--- src/pages/Edit.tsx
+++ src/pages/Edit.tsx
@@ -1,33 +1,14 @@
-import { useContext, useEffect, useState } from "react";
-import { useNavigate, useParams, useSearchParams } from "react-router-dom";
-import { DiaryStateContext, StateType } from "../App";
 import DiaryEditor from "../components/DiaryEditor";
+import useDiary from "../hooks/useDiary";
 
 const Edit = () => {
-  const navigate = useNavigate();
-  const { id } = useParams(); // string 또는 파라미터없는경우 undefined 반환 // useParams 는 파라미터 /user/:id // useSearchParams 는 ? 쿼리파라미터 값 가져옴
-  // const [searchParams, setSearchParams] = useSearchParams(); // 쿼리스트링 바꾸기 setSearchParams({id: "eddddd"})
+	const diary = useDiary();
 
-  const diaryList = useContext(DiaryStateContext);
-  const [diary, setDiary] = useState<StateType>();
-
-  useEffect(() => {
-    // 화면렌더시 자동실행
-    if (diaryList.length > 1) {
-      // 일기가 1개 이상일때 실행
-      const data = diaryList.find((item) => item.id === parseInt(id!)); // id! == id 파라미터가 무조건 있음을 의미
-			console.log(data);
-			if (data) {
-        setDiary(data);
-      } else {
-        // url 의 id 가 없는 데이터의 경우 undefined(==false) 반환
-        navigate("/", { replace: true });
-      }
-    }
-  }, [diaryList, id]); // diaryList || id 가 변경되는 경우에 재실행 // 일기 데이터가 변경되는 경우에 재실행
-
+  if(!diary) {
+		return <div>로딩중입니다...</div>
+	}
 	// 수정할 다이어리가 있으면, 수정컴포넌트를 보여주겠다.
-  return <div>{diary && <DiaryEditor isEdit={true} writeDiary={diary} />}</div>;
+	return <div>{diary && <DiaryEditor isEdit={true} writeDiary={diary} />}</div>;
 };
 
 export default Edit;
